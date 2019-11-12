4e12143f51b4b860cdc1d1e8e2c3aaa033e23472
chudnofskyATD <mpereannor@gmail.com>
2019-11-12 18:14:23 +0000

    users tested successfully with sessions and cookies

--- api/server.js
+++ api/server.js
@@ -3,9 +3,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const session = require('express-session');
-
-const AuthRouter = require('../auth/auth-router');
-const UserRouter = require('../users/users-router')
+const KnexSessionStore = require('connect-session-knex')(session);
+const authRouter = require('../auth/auth-router');
+const userRouter = require('../users/users-router')
 
 
 const server = express();
@@ -19,7 +19,14 @@ const sessionConfig = {
         httpOnly: false,
     },
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new KnexSessionStore({ 
+        knex: require('../database/dbConfig'),
+        tablename: 'sessons',
+        sidefieldname: 'sid',
+        createTable: true,
+        clearInterval: 1000 * 60 * 60
+    })
 }
 
 server.use(helmet());
@@ -28,7 +35,7 @@ server.use(cors());
 server.use(session(sessionConfig));
 
 
-server.use('/api', UserRouter);
-server.use('/api/auth', AuthRouter);
+server.use('/api', userRouter);
+server.use('/api/auth', authRouter);
 
 module.exports = server;
